ac2ea9c4d97f929581f1211e72703a7d9c5158f8
Prasad Kute <prasadkute0708@gmail.com>
2025-02-06 08:12:31 +0530

    /home1 404 not found error solved...

--- client/web/app/components/ProtectedRoutes.tsx
+++ client/web/app/components/ProtectedRoutes.tsx
@@ -1,23 +1,10 @@
 import { Navigate, Outlet } from "react-router";
 import { useSelector } from "react-redux";
 import type { RootState } from "../redux/store";
-import { useEffect, useState } from "react";
+
 
 export default function ProtectedRoutes() {
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-  const [isClient, setIsClient] = useState(false);
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-50">
-        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-purple-600"></div>
-      </div>
-    </>; // a loading spinner
-  }
 
   return isAuthenticated ? <Outlet /> : <Navigate to="/home1" replace />;
 }
